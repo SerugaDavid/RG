a3742413dfdf8b880da1b21ccd58b8722120003e
davidseruga <56600501+davidseruga@users.noreply.github.com>
2021-12-17 18:28:49 +0100

    Update for DN3

--- DN3/QuadTree.js
+++ DN3/QuadTree.js
@@ -1,27 +1,32 @@
+import {Vector} from "./Vector.js";
+import {Point} from "./Point.js";
+import {Rectangle} from "./Rectangle.js";
+
 class QuadTree {
-    constructor(points, max) {
-        this.points = points;
-        this.children = [null, null, null, null];
-        this.max = max;
-    }
+  constructor(points, max, rectangle) {
+    this.rectangle = rectangle;
+    this.points = points;
+    this.children = [null, null, null, null];
+    this.max = max;
+  }
 
-    capacity() {
-        // Method that returns the current capacity of the QuadTree node.
-        return this.points.length;
-    }
+  capacity() {
+    // Method that returns the current capacity of the QuadTree node.
+    return this.points.length;
+  }
 
-    addPoint(point) {
-        // Method that adds a point to the current QuadTree node.
-        this.points.push(point);
-        if (this.capacity() > this.max){
-            this.split();
-        }
+  addPoint(point) {
+    // Method that adds a point to the current QuadTree node.
+    this.points.push(point);
+    if (this.capacity() > this.max){
+        this.split();
     }
+  }
 
-    split() {
-        // Method for spliting the QuadTree node to 4 other nodes.
-        for (let i = 0; i < 4; i++) {
-            this.children[i] = new QuadTree([new Point], this.max);
-        }
+  split() {
+    // Method for spliting the QuadTree node to 4 other nodes.
+    for (let i = 0; i < 4; i++) {
+        this.children[i] = new QuadTree([new Point], this.max);
     }
+  }
 }
